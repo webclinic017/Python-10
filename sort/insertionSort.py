c58ff1afda0aebded052a4f138021954e5c088f1
minkj1992 <37536298+minkj1992@users.noreply.github.com>
2019-01-15 09:04:57 +0900

    Update insertionSort.py

--- sort/insertionSort.py
+++ sort/insertionSort.py
@@ -1,1 +1,19 @@
+#first try
+#TIL: insertioin sort를 활용하여 insert 될때마다 
+#TIL: reversed(s) == s[::-1]
+#TIL: print(*list,sep='\n')
+#TIL: s.insert(index,value)
 
+s = []
+for _ in range(int(input())):
+  if len(s)==0:
+    s.append(int(input()))
+  else:
+  #맨 뒤에서 부터 index를 빼주면서 전진한다.
+    ins,indx = int(input()),len(s)
+    for i in s[::-1]:
+      if ins>=i:
+        break
+      indx-=1
+    s.insert(indx,ins)
+print(*s,sep='\n')
